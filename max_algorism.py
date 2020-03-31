d17b4fd93bc5f2fef45d4b986ba9f5cd14ccb787
dlgoslaek1 <dlgoslaek1@gmail.com>
2020-03-31 22:01:50 +0900

    edit_error

--- max_algorism.py
+++ max_algorism.py
@@ -1,34 +1,27 @@
 import random
 
-def alg1(A):
-    Max = [0]*(n)
-    Max[0] = A[0]
+def alg1() :
+	Max = [0] * (n)
+	for i in range(0, n) :
+		for j in range(0, i + 1) :
+			if (Max[i] < A[j]) :
+				Max[i] = A[j]
+	return Max
 
-    for i in range(1,n+1):
-        for j in range(0,i):
-            if(Max[j] < A[j]):
-                Max[i-1] = A[j]
-                print(Max)
-                
-    return Max
-            
-
-def alg2(A):
-    Max = [0]*n
-    for i in range(0,n):
-        M
-    return alg2(A)
-        
-
+def alg2() :
+	Max = [0] * n
+	Max[0] = A[0]
+	for i in range(0, n - 1) :
+		Max[i + 1] = max([Max[i], A[i + 1]])
+	return Max
 
 n = input("리스트 A의 원소의 개수를 입력하세요: ")
 n = int(n)
 A = list()
 
-for i in range(0,n):
-    A.append(random.randrange(1,100))
+for i in range(0, n) :
+    A.append(random.randrange(1, 100))
 
 print("리스트 A의 원소:", A)
-
-print(alg1(A))
-print(alg2(A))
+print("alg1으로 구한 최댓값 리스트: ", alg1())
+print("alg2으로 구한 최댓값 리스트: ", alg2())
