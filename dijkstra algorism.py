0945935846745c98538f283d1d18659c22df5da2
dlgoslaek1 <dlgoslaek1@gmail.com>
2020-05-13 17:16:42 +0900

    update save_length

--- dijkstra algorism.py
+++ dijkstra algorism.py
@@ -5,7 +5,7 @@ n=5
 f=set()
 touch=n*[0]
 length=n*[0]
-
+save_length=n*[0]
 
 for i in range(1,n):
     length[i] = w[0][i]
@@ -20,28 +20,19 @@ for i in range(1,n):
             vnear = j
 
     e = (touch[vnear], vnear)
+    save_length[n-i] = length[vnear]
     f.add(e)
-
     for j in range(1,n):
         if(length[vnear] + w[vnear][j] < length[j]):
             length[j] = length[vnear] + w[vnear][j]
             touch[j] = vnear
+            
     length[vnear] = -1
 
 print(f)
 
 
-inf=1000
-w=[[0,7,4,6,1],[inf,0,inf,inf,inf],
-   [inf,2,0,5,inf], [inf,3,inf,0,inf], [inf,inf,inf,1,0]]
-n=5
-f=set()
-touch=n*[0]
-length=n*[0]
-save_length=n*[0]
 
-
-#구현
 
 print(save_length) 
 
